68e425d4d9deb28ecf3d27678eeda4ece9968174
MarcusNeto <marcusnetoo@outlook.com>
2025-02-25 10:13:59 -0300

    upload cmcconsult project

---
+++ CMCCONSULT/javascript/index.js
@@ -0,0 +1,25 @@
+function redirectToPage() {
+    window.open("checklist/opcoes.html", "_blank"); 
+}
+
+function redirectToPage2() {
+    window.open("cadastro.html", "_blank"); 
+}
+
+function redirectToPage3() {
+    window.open("senhas.html", "_blank"); 
+}
+
+function redirectToPage4() {
+    window.open("links.html", "_blank"); 
+}
+
+function increaseSize(element) {
+    element.classList.add("aumentar");
+}
+
+function resetSize(element) {
+    element.classList.remove("aumentar");
+}
+
+/*==================================================================================================================*/
